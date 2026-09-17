d94fe71dce73f972def23a8cd4f9d68bcf61c64a
isabelasantorini2548 <isabelasantorini2548@gmail.com>
2026-09-17 01:07:22 -0400

    chore: eliminar scripts de prueba

--- check.js
+++
@@ -1,9 +0,0 @@
-// Simple check
-const fs = require('fs');
-const path = require('path');
-const layoutsDir = path.join(__dirname, 'uploads', 'layouts');
-console.log('Layouts dir exists:', fs.existsSync(layoutsDir));
-if (fs.existsSync(layoutsDir)) {
-  const files = fs.readdirSync(layoutsDir);
-  console.log('Files in layouts:', files);
-}
